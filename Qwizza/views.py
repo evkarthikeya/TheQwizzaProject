e954ee5680b77c0711007e21fc0cbfe1b4e1a7a0
KarthikeyaEV <101663062+KarthikeyaEV@users.noreply.github.com>
2022-08-06 10:54:45 +0530

    Added the component calls in Views.py

--- Qwizza/views.py
+++ Qwizza/views.py
@@ -4,3 +4,22 @@ from django.shortcuts import render
 # Create your views here.
 def homepage(request):
     return render(request, "Homepage.html",{})
+def discussion(request):
+    return render(request, "discussion.html",{})
+def signup(request):
+    return render(request, "signup.html",{})
+def sub_english(request):
+    return render(request, "english.html",{})
+def sub_math(request):
+    return render(request, "Math.html",{})
+def sub_physci(request):
+    return render(request, "Physicalscience.html",{})
+def sub_social(request):
+    return render(request, "socialstudies.html",{})
+def type_fib(request):
+    return render(request, "FIB.html",{})
+def type_tof(request):
+    return render(request, "TrueOrFalse.html",{})
+def type_mcq(request):
+    return render(request, "QuestionMCQs.html",{})
+
